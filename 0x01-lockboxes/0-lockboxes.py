aca4ac9b084b54f1a8ac7fe94e1a4768d835bcf7
Sottie1 <sottiegadson@gmail.com>
2023-01-31 12:06:17 +0000

    initial commit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,16 @@
 def canUnlockAll(boxes):
+    """
+    A function that determines if all boxes can be opened.
+    
+    Given a list of boxes, where each box is numbered sequentially from 0 to n-1, 
+    and each box may contain keys to open other boxes, the function returns 
+    True if all boxes can be opened, and False otherwise.
+    
+    :param boxes: a list of lists where each list represents a box
+    :type boxes: list
+    :return: True if all boxes can be opened, False otherwise
+    :rtype: bool
+    """
     n = len(boxes)
     keys = {0}
     for i in range(n):
